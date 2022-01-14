01fe87e066ca4f71971eb02649e615c548304b38
[firstname <[tuannaph14620@fpt.edu.vn]>
2022-01-14 18:30:36 +0700

    y

--- src/pages/admin/news/indexNews.js
+++ src/pages/admin/news/indexNews.js
@@ -5,7 +5,26 @@ const indexNews = {
         render() {
             return /*html*/ `
         ${headerAdmin.render()}
-        
+        <div class="lg:flex lg:items-center lg:justify-between pt-10">
+        <div class="flex-1 min-w-0">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                Quản lý bài viết
+
+            </h2>
+
+        </div>
+        <div class="mt-5 flex lg:mt-0 lg:ml-4">
+            <span class="sm:ml-3">
+            <a href="/admin/news/add">
+            <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <!-- Heroicon name: solid/check -->
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              Thêm mới
+            </button>
+            </a>
+          </span>
+        </div>
+    </div>
         <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
